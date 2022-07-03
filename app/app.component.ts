2e8a89faece854307917bb58aa1189791ac4b5c7
sarveshped30 <sarveshpednekar30@gmail.com>
2022-07-03 18:48:10 +0530

    Modified: App component to use event binding technique to bind click event to launch bridgelabz url on new tab

--- app/app.component.ts
+++ app/app.component.ts
@@ -8,8 +8,16 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title:string = 'HelloWorld';
   imgUrl:string = "assets/images/BridgeLabz-Logo.jpg"
+  url:string = "https://www.bridgelabz.com";
 
+  //On application start will initialize title to "Hello from bridgelabz" 
   ngOnInit() : void {
     this.title = "Hello from bridgelabz";
+  }
+
+  //Clicking on bridgelabz logo will trigger event and redirect to bridgelabz website  
+  onClick($event : any) {
+    console.log("Save button is clicked",$event);
+    window.open(this.url);
   }
 }
